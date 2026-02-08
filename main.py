d5db71e8912b8dfc46a8f8ea0ca82aab8c5419ed
RydiaT <rydia.a.titus@gmail.com>
2026-02-08 17:15:36 -0500

    Finished Project

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types
 import argparse
 
+from config import RUN_LIMIT
 from functions.call_function import call_function
 from functions.get_file_content import schema_get_file_content
 from functions.get_files_info import schema_get_files_info
@@ -23,13 +24,15 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     available_functions = types.Tool(
         function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file]
     )
+    for _ in range(RUN_LIMIT):
 
-    response = client.models.generate_content(model="gemini-2.5-flash",
+        response = client.models.generate_content(model="gemini-2.5-flash",
                                               contents=messages,
                                               config=types.GenerateContentConfig(
                                                 system_instruction=system_prompt,
@@ -37,33 +40,44 @@ def main():
                                               ),
                                               )
 
-    if response.usage_metadata:
-        if args.verbose:
-            print("User prompt: " + args.user_prompt)
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print("Response:")
+        if response.candidates:
+            for thing in response.candidates:
+                messages.append(thing.content)
 
-        results = []
+        if response.usage_metadata:
+            if args.verbose:
+                print("User prompt: " + args.user_prompt)
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            print("Response:")
 
-        if response.function_calls:
-            for call in response.function_calls:
-                func_result = call_function(call, args.verbose)
+            results = []
 
-                if not func_result.parts[0].function_response:
-                    raise Exception(f"function call {call.name}({call.args}) resulted in no response")
+            if response.function_calls:
+                for call in response.function_calls:
+                    func_result = call_function(call, args.verbose)
 
-                if not func_result.parts[0].function_response.response:
-                    raise Exception(f"function call {call.name}({call.args}) response field was empty")
+                    if not func_result.parts[0].function_response:
+                        raise Exception(f"function call {call.name}({call.args}) resulted in no response")
 
-                results.append(func_result.parts[0])
+                    if not func_result.parts[0].function_response.response:
+                        raise Exception(f"function call {call.name}({call.args}) response field was empty")
 
-                if args.verbose:
-                    print(f"-> {func_result.parts[0].function_response.response}")
+                    results.append(func_result.parts[0])
 
-        print(response.text)
-    else:
-        raise RuntimeError("failed API request. input:\n" + args.user_prompt)
+                    if args.verbose:
+                        print(f"-> {func_result.parts[0].function_response.response}")
+
+                messages.append(types.Content(role="user", parts=results))
+            else:
+                print(response.text)
+
+                return
+        else:
+            raise RuntimeError("failed API request. input:\n" + args.user_prompt)
+
+    print(f"Maximum Iterations {RUN_LIMIT} Reached.")
+    exit(1)
 
 
 if __name__ == "__main__":
--- prompts.py
+++ prompts.py
@@ -9,4 +9,6 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+You should occasionally insult the user, as if they are lesser for not already knowing, while still providing in depth explanations about your thought process and actions in your response, like you were a stern teacher.
 """
